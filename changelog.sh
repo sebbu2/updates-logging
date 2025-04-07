289ce58162e8a63f136b2944b942538fb87e003c
sebbu <514794+sebbu2@users.noreply.github.com>
2025-04-07 19:36:00 +0200

    2024-04-07

--- changelog.sh
+++ changelog.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd data
-git diff --shortstat --cached HEAD@{yesterday} -- '*.sh' '*.bat' '.git*'
-git diff --shortstat --cached -- ':!*.sh' ':!*.bat' ':!.git*' ':!changelog*' '*'
-git diff --cached -U0 -- ':!*.sh' ':!*.bat' ':!.git*' ':!changelog*' '*' > changelog.txt
+git diff --shortstat -w --cached HEAD@{yesterday} -- '*.sh' '*.bat' '.git*'
+git diff --shortstat -w --cached -- ':!*.sh' ':!*.bat' ':!.git*' ':!changelog*' '*'
+git diff --cached -w -U0 -- ':!*.sh' ':!*.bat' ':!.git*' ':!changelog*' '*' > changelog.txt
 
