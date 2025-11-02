38eab67c8f1ab96fede7112e9200c071decde853
sebbu <514794+sebbu2@users.noreply.github.com>
2025-11-02 21:17:11 +0100

    2025-10-02

--- run-topgrade.bat
+++ run-topgrade.bat
@@ -2,6 +2,7 @@
 set DIR2="%~dp0"
 cd /d %userprofile%
 topgrade.exe --disable wsl --disable wsl_update --disable vcpkg
+call scoop reset diffutils dos2unix gawk grep innounp patch sed vim wget innounp >NUL
 call scoop cleanup -k -a
 call %DIR2%\mv-lnk.bat
 
